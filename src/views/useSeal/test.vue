cbe9f4dc69a4667f270d34ffdaa87edda29cb831
soumns <2794122209@qq.com>
2022-06-06 17:36:39 +0800

    ✨ feat:change-test

--- src/views/useSeal/test.vue
+++ src/views/useSeal/test.vue
@@ -1,9 +1,15 @@
 <template>
-  <div class="container-top">
-    <div class="img">
-      <img src="@/assets/uploads/yongyin-icon.png" alt="" />
+  <div class="container">
+    <div class="container-top">
+      <div class="img">
+        <img src="@/assets/uploads/yongyin-icon.png" alt="" />
+      </div>
+      <span>用印申请20220428011</span>
     </div>
-    <span>用印申请20220428011</span>
+
+    <van-cell-group>
+      <van-cell title="单元格" value="1313139012390910390129121" />
+    </van-cell-group>
   </div>
 </template>
 
@@ -19,20 +25,32 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.container-top {
-  display: flex;
-  align-items: center;
+.container {
+  .container-top {
+    display: flex;
+    align-items: center;
 
-  .img {
-    width: 23px;
-    height: 28px;
-    img {
-      width: 100%;
+    .img {
+      width: 23px;
+      height: 28px;
+      img {
+        width: 100%;
+      }
+    }
+
+    span {
+      font-size: 30px;
     }
   }
+}
 
+/* 调整内容最小宽度 */
+::v-deep .van-cell__value {
+  min-width: 70%;
   span {
-    font-size: 30px;
+    display: inline-block;
+    text-align: left;
+    word-break: break-all;
   }
 }
 </style>
